9cc829f9d3f33079ca9db4c8047eb36ede079cc6
genkokudo <y-toyoda@live.jp>
2026-06-29 15:09:25 +0900

    関数追加。ブラウザ用とサイト用

--- PortfolioUtilityFunctions/Function1.cs
+++ PortfolioUtilityFunctions/Function1.cs
@@ -7,10 +7,17 @@ namespace PortfolioUtilityFunctions;
 
 public class Function1(ILogger<Function1> logger)
 {
-    [Function("Test")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    [Function("TestFromSite")]
+    public IActionResult RunTest([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
     {
         logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!🐧");
+        return new OkObjectResult("Welcome to Azure Functions1!🐧");
+    }
+
+    [Function("TestFromBrowser")]
+    public IActionResult RunTest2([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions2!🐧");
     }
 }
