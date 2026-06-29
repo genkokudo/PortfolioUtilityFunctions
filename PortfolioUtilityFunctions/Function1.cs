13cf9b4f5ffb260347711cb83f0bea9daefaf3e6
genkokudo <y-toyoda@live.jp>
2026-06-29 16:07:14 +0900

    Anonymousにする。

--- PortfolioUtilityFunctions/Function1.cs
+++ PortfolioUtilityFunctions/Function1.cs
@@ -7,8 +7,9 @@ namespace PortfolioUtilityFunctions;
 
 public class Function1(ILogger<Function1> logger)
 {
+    // Blazor WebAssembly (WASM)は環境変数も解析で読まれる可能性があるので、AnonymousにしてCORSでドメイン制限をかける。WASMからアクセスする関数は、関数キー方式は使わない。
     [Function("TestFromSite")]
-    public IActionResult RunTest([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    public IActionResult RunTest([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
     {
         logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult("Welcome to Azure Functions1!🐧");
