177bba9c8cd0a4e0baaf7259624d91003d603505
genkokudo <y-toyoda@live.jp>
2026-07-22 23:53:58 +0900

    GetProfileAsyncを追加

--- PortfolioUtilityFunctions/GetPortfolioData.cs
+++ PortfolioUtilityFunctions/GetPortfolioData.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Portfolio.Shared.Model;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace PortfolioUtilityFunctions
 {
@@ -27,17 +28,41 @@ namespace PortfolioUtilityFunctions
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
         {
+            var profile = await GetProfileAsync();
             var result = new PortfolioData
             {
-                AuthorNameKanji = Environment.GetEnvironmentVariable("AuthorNameKanji") ?? string.Empty,
-                AuthorNameHiragana = Environment.GetEnvironmentVariable("AuthorNameHiragana") ?? string.Empty,
-                AuthorNameAlphabet = Environment.GetEnvironmentVariable("AuthorNameAlphabet") ?? string.Empty,
+                AuthorNameKanji = profile?.AuthorNameKanji ?? string.Empty,
+                AuthorNameHiragana = profile?.AuthorNameHiragana ?? string.Empty,
+                AuthorNameAlphabet = profile?.AuthorNameAlphabet ?? string.Empty,
+                ProfileImageUrl = profile?.ProfileImageUrl ?? string.Empty,
+                ProfessionalTitle = profile?.ProfessionalTitle ?? string.Empty,
+                ProfileDescription = profile?.ProfileDescription ?? string.Empty,
+                GitHubUrl = profile?.GitHubUrl ?? string.Empty,
+                ProfileTags = profile?.ProfileTags ?? new List<string>(),
                 Skills = await GetSkillsAsync(),
                 WorkHistories = await GetWorkHistoriesAsync(),
                 Works = await GetWorksAsync()
             };
 
             return new OkObjectResult(result);
+        }
+
+        /// <summary>
+        /// Profileを取得する
+        /// </summary>
+        /// <returns></returns>
+        private async Task<ProfileItem?> GetProfileAsync()
+        {
+            var container = cosmosClient.GetContainer(_databaseId, "Profile");
+            try
+            {
+                var response = await container.ReadItemAsync<ProfileItem>("profile", new PartitionKey("profile"));
+                return response.Resource;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return null; // ドキュメントがまだ無い場合
+            }
         }
 
         /// <summary>
